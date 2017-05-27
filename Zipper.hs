05334e43adc653e7eae8808a10df21699cf12051
Rodney Pilgrim <rodneyp290@gmail.com>
2017-05-27 11:10:50 +1000

    FIX: Add brackets to pattern matching to remove ambiguity causing "different numbers of arguments" error

--- Zipper.hs
+++ Zipper.hs
@@ -12,26 +12,26 @@ zipperFromList (h:t) = Zipper [] h t
 
 cursorSafe :: Zipper a -> Maybe a
 cursorSafe Zip = Nothing
-cursorSafe Zipper _ c _ = Just c
+cursorSafe (Zipper _ c _) = Just c
 
 nextSafe :: Zipper a -> Maybe (Zipper a)
 nextSafe Zip = Nothing
-nextSafe Zipper _ _ [] = Nothing
-nextSafe Zipper l c (rh:rt) = Just (Zipper (c:l) rh rt)
+nextSafe (Zipper _ _ []) = Nothing
+nextSafe (Zipper l c (rh:rt)) = Just (Zipper (c:l) rh rt)
 
 prevSafe :: Zipper a -> Maybe (Zipper a)
 prevSafe Zip = Nothing
-prevSafe Zipper [] _ _ = Nothing
-prevSafe Zipper (lh:lt) c r = Just (Zipper lt lh (c:r) )
+prevSafe (Zipper [] _ _) = Nothing
+prevSafe (Zipper (lh:lt) c r) = Just (Zipper lt lh (c:r) )
 
 insertBefore :: a -> Zipper a -> Zipper a
 insertBefore e Zip = zipper e
-insertBefore e Zipper l c r = Zipper (c:l) e r
+insertBefore e (Zipper l c r) = Zipper (c:l) e r
 
 insertAfter :: Zipper a -> Zipper a
 insertAfter e Zip = zipper e
-insertAfter e Zipper l c r = Zipper l e (c:r)
+insertAfter e (Zipper l c r) = Zipper l e (c:r)
 
 replace :: a -> Zipper a -> Zipper a
 replace e Zip = error "Can't replace cursor of empty zipper"
-replace e Zipper l _ r = Zipper l e r
+replace e (Zipper l _ r) = Zipper l e r
